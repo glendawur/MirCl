b13be2bae4080dfc7b05a7284617203801886f72
Andrei Rykov <48521539+glendawur@users.noreply.github.com>
2022-10-03 23:15:06 +0200

    bug fixed!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='clusterlib',
-    packages=find_packages(include=['clusterlib']),
+    packages=find_packages(),
     version='0.1.0',
     description='Various clustering techniques',
     author='glendawur',
