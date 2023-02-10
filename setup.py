6bb3ebf9e27668342ed3a2188433974efe778967
Andrei Rykov <andreyrykovspb@gmail.com>
2023-02-10 23:08:36 +0100

    Changes in setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='clusterlib',
+    name='MirCl',
     packages=find_packages(),
     version='0.1.0',
-    description='Various clustering techniques',
+    description='MirCl [Miracle Clustering]. Various clustering techniques',
     author='glendawur',
     license='MIT'
 )
